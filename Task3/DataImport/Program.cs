69095f9bb7f687f7ce33e01be704be5af5026f51
Dejknox <knoxter_uk@hotmail.com>
2022-12-20 15:37:30 +0000

    Task 3

---
+++ Task3/DataImport/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DataImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("..//..//..//TechnicalTestData.csv").Select(x => x.Split(","));
+
+            var petrolTypes = data.OrderBy(x => x[4]).Select(x => x[4] == "Petrol");
+            File.WriteAllText("..//..//..//Petrol.csv", petrolTypes.ToString());
+
+            var electricTypes = data.Select(x => x[4] == "Electric");
+            File.WriteAllText("..//..//..//Electric.csv", electricTypes.ToString());
+
+            var dieselTypes = data.Select(x => x[4] == "Diesel");
+            File.WriteAllText("..//..//..//Diesel.csv", dieselTypes.ToString());            
+        }
+    }
+}
