f764a9ae3854473c4a8722c4a6b5f3c5c3c79c75
Dejknox <knoxter_uk@hotmail.com>
2022-12-20 15:37:19 +0000

    Task 1

---
+++ Task1/AnagramTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace AnagramTest
+{
+    internal class Program
+    {
+        static private bool IsAnagram(string first, string second)
+        {
+            return first.OrderBy(f => f).SequenceEqual(second.OrderBy(s => s));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string:");
+            string firstString = Console.ReadLine();
+
+            Console.WriteLine("Enter second string:");
+            string secondString = Console.ReadLine();
+
+            bool result = IsAnagram(firstString, secondString);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
