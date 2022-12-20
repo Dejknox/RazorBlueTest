4997ca500b1c863a2bb23c56c93860005b673cda
Dejknox <knoxter_uk@hotmail.com>
2022-12-20 16:10:49 +0000

    Task 2

---
+++ Task2/IPFiltering/Program.cs
@@ -0,0 +1,37 @@
+﻿using NetTools;     // Use third party library
+using System;
+using System.Collections.Generic;
+
+namespace IPFiltering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPAddressRange> ipList = new List<IPAddressRange>();
+
+            ipList.Add(IPAddressRange.Parse("192.168.0.1 - 192.168.0.10"));
+            ipList.Add(IPAddressRange.Parse("192.168.0.30"));
+            ipList.Add(IPAddressRange.Parse("192.168.0.50/10"));
+
+            Console.WriteLine("Please enter a valid Ipv4 address:");
+            var ipToFind = Console.ReadLine();
+
+            bool ipFound = false;
+            foreach (IPAddressRange item in ipList)
+            {
+                if (item.Contains(IPAddressRange.Parse(ipToFind)))
+                {
+                    Console.WriteLine("IP is in the List!");
+                    ipFound = true;
+                    break;
+                }
+            }
+
+            if (!ipFound)
+            {
+                Console.WriteLine("IP is not in the list!");
+            }
+        }
+    }
+}
